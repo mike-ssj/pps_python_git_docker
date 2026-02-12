ae86b2713a83bf7b22e080c363a108da0f49dd52
Miguel Sánchez Campillo <1848487@alu.murciaeduca.es>
2026-02-12 20:04:35 +0100

    Nuevo endopoint para añadir frases y comprobada la persistencia

--- bayeta.py
+++ bayeta.py
@@ -1,5 +1,7 @@
-import random
-from frases_mongo import consultar
+from frases_mongo import consultar, add
 
 def frotar(n_frases: int = 1) -> list:
     return consultar(n_frases)
+
+def add_frases(lista_frases: list):
+    add(lista_frases)
